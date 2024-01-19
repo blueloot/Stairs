f6a083e4133646560ef27ee6ec23ac3909f472bb
blueloot <goatfishgames@gmail.com>
2024-01-19 18:05:29 +0100

    improve documentation

--- C#/LoadingScreen/scene1.cs
+++ C#/LoadingScreen/scene1.cs
@@ -5,10 +5,21 @@ public partial class scene1 : Control
 {
 	public override void _Ready()
 	{
-		var btn = GetNodeOrNull<Button>("Button");
 
+		// disable the replace root mode.
+		// this is used to tell the loading screen to add itself as a child of root
+		// if this is set to true, the loading screen will replace the root scene
+		SceneManager.SetReplaceRootMode(true);
+
+		// set the active scene to this scene to tell the loading screen which scene it should queue free
+		// SceneManager.ActiveScene = this;
+
+
+		// what scene to load on button press
 		var newScene = "res://C#/LoadingScreen/scene2.tscn";
 
+		// get the button and add a pressed signal
+		var btn = GetNodeOrNull<Button>("Button");
 		btn.Pressed += () => SceneManager.Load(newScene);
 	}
 }
